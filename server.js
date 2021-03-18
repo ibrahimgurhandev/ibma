411ad13ea7f74ef94496f43f967e1ef66cb06bc5
ChuHiStand <marcobragado.cs@gmail.com>
2021-03-18 02:06:41 -0700

    chatbot congfigured to say what room you currently are in"

--- server.js
+++ server.js
@@ -1,111 +1,119 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const socketio = require('socket.io');
+const http = require("http");
+const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
-const { setUpRoutes }= require("./app/routes.js")
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const morgan = require('morgan');
-const session = require('express-session');
-const configDB = require('./config/database.js');
+const { setUpRoutes } = require("./app/routes.js");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const morgan = require("morgan");
+const session = require("express-session");
+const configDB = require("./config/database.js");
 const RoomService = require("./app/services/roomService");
 const UserService = require("./app/services/userServices");
 
-let db
+let db;
 //test
 // configuration ===============================================================
 mongoose.connect(configDB.url, (err, database) => {
-  if (err) return console.log(err)
-  db = database
+  if (err) return console.log(err);
+  db = database;
   setUpRoutes(app, passport, db);
-}); 
-mongoose.set('useFindAndModify', false);
+});
+mongoose.set("useFindAndModify", false);
 
-require('./config/passport')(passport); // pass passport for configuration
+require("./config/passport")(passport); // pass passport for configuration
 
 // set up our express application
-app.use(morgan('dev')); // log every request to the console
+app.use(morgan("dev")); // log every request to the console
 app.use(express.json()); // get information from html forms
-app.use(express.urlencoded({
-  extended: true
-}));
-app.use(express.static('public'))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set("view engine", "ejs"); // set up ejs for templating
 
 // required for passport
-app.use(session({
-  secret: 'rcbootcamp2019a', // session secret
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "rcbootcamp2019a", // session secret
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // launch ======================================================================
 
-const botName = 'Bonodero the Welcome Bot';
+const botName = "Bonodero the Welcome Bot";
 
-const formatMessage = require('./app/utils/messages');
+const formatMessage = require("./app/utils/messages");
 const {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers
-} = require('./app/utils/users');
-const UserSchema = require('./app/models/UserSchema.js');
+  getRoomUsers,
+} = require("./app/utils/users");
+const UserSchema = require("./app/models/UserSchema.js");
 
 // Run when client connects
-io.on('connection',(socket) => {
-  socket.on('joinRoom', async ({ username, room }) => {
-   // Deleted usernamne and replaced line 74 user.name with username
-    await UserService.updateRoomAndSocket(username, room, socket.id)
-    .catch(err=> console.log("Error Joining user to socket",err))
+io.on("connection", (socket) => {
+  socket.on("joinRoom", async ({ username, room }) => {
+    // Deleted usernamne and replaced line 74 user.name with username
+    await UserService.updateRoomAndSocket(
+      username,
+      room,
+      socket.id
+    ).catch((err) => console.log("Error Joining user to socket", err));
     socket.join(room);
-    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+    socket.emit("message", formatMessage(botName, `Welcome to ${room} room!`));
     socket
       .to(room)
       .emit(
-        'message',
+        "message",
         formatMessage(botName, `${username} has joined the chat`)
-      )
-    io.to(room).emit('roomUsers', {
-        room: room,
-        users: await UserService.getByRoom(room)
-        .catch(err=>console.log("Error in room user broadcast: ", err))
-      });
+      );
+    io.to(room).emit("roomUsers", {
+      room: room,
+      users: await UserService.getByRoom(room).catch((err) =>
+        console.log("Error in room user broadcast: ", err)
+      ),
+    });
   });
 
-  socket.on('chatMessage', async (msg, username) => {
+  socket.on("chatMessage", async (msg, username) => {
     const user = await UserService.getByName(username);
-    RoomService.addMessage(user.room, {userId: user._id, text: msg})
-    io.to(user.room).emit('message', formatMessage(user.name, msg));
-
+    RoomService.addMessage(user.room, { userId: user._id, text: msg });
+    io.to(user.room).emit("message", formatMessage(user.name, msg));
   });
 
-  socket.on('disconnect', async () => {
-    const user = await UserService.clearRoomAndSocket(socket.id)
-    .catch(err => console.log(" error in disconnect" , err))
+  socket.on("disconnect", async () => {
+    const user = await UserService.clearRoomAndSocket(socket.id).catch((err) =>
+      console.log(" error in disconnect", err)
+    );
     if (user) {
       io.to(user.room).emit(
-        'message',
+        "message",
         formatMessage(botName, `${user.name} has left the chat`)
       );
 
       // Send users and room info
-      io.to(user.room).emit('roomUsers', {
+      io.to(user.room).emit("roomUsers", {
         room: user.room,
-        users: await UserService.getByRoom(user.room)
-        .catch(err=>console.log("Error in roomusers Evcent on disconnect", err))
+        users: await UserService.getByRoom(user.room).catch((err) =>
+          console.log("Error in roomusers Evcent on disconnect", err)
+        ),
       });
     }
   });
 });
-
 
 const PORT = process.env.PORT || 3000;
 
